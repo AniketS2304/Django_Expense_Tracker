eea1b3241cbcc38c2583bc31264e6b8c7cfc12ee
Aniket Suryavanshi <aniketsuryavanshi2304@gmail.com>
2026-01-07 00:33:23 +0530

    render database conn change

--- Expence_Tracker/settings.py
+++ Expence_Tracker/settings.py
@@ -96,6 +96,18 @@ DATABASES = {
 }
 
 
+import os
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
@@ -144,3 +156,4 @@ MEDIA_URL = '/media/'
 #     "*",
 #     "django-expense-tracker-1-h4lo.onrender.com",
 # ]
+print("FINAL DATABASE ENGINE:", DATABASES["default"]["ENGINE"])
